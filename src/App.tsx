5a1127198e410da1b7bec7ecb289be32536a845f
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-18 17:09:48 +0900

    Add : App 컴포넌트 레이아웃 구성 (MobileView)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import MobileView from './templates/MobileView';
 
 function App() {
   return (
-    <>
+    <MobileView>
       <Header />
       <Outlet />
       <NavBar />
-    </>
+    </MobileView>
   );
 }
 
