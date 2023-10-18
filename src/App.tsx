9e4b0220a6262936d450740738dce29105318301
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-18 18:13:45 +0900

    Modify : Header 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './organisms/Header';
 import NavBar from './components/NavBar';
 import MobileView from './templates/MobileView';
 
