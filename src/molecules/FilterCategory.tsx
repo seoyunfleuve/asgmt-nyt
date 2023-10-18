22b356a2f8c7afd3fc54acd0ca21913b114a514b
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-19 08:40:12 +0900

    Add : FilterCategory 컴포넌트 추가

---
+++ src/molecules/FilterCategory.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { subtitleFont } from '../styles/Font.style';
+import FilterInput from '../atoms/FilterInput';
+import { IFilterCategoryProps } from '../type/type';
+import FilterTab from './FilterTab';
+
+export default function FilterCategory({
+  title,
+  placeholder,
+  isInput,
+}: IFilterCategoryProps) {
+  return (
+    <FCategory>
+      <CategoryTitle>{title}</CategoryTitle>
+      {isInput ? (
+        <FilterInput placeholder={placeholder} />
+      ) : (
+        <FilterTab type="country" />
+      )}
+    </FCategory>
+  );
+}
+
+const FCategory = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const CategoryTitle = styled.p`
+  ${subtitleFont}
+`;
