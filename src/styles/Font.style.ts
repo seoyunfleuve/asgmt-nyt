12c68557a4e0b04ab9514896c12bd96604fa5717
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-19 07:56:55 +0900

    Add : Font.style.ts 추가

---
+++ src/styles/Font.style.ts
@@ -0,0 +1,29 @@
+import { css } from 'styled-components';
+
+export const titleFont = css`
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 28px;
+  letter-spacing: -0.05em;
+`;
+
+export const subtitleFont = css`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: -0.05em;
+`;
+
+export const innerFont = css`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: -0.04em;
+`;
+
+export const captionFont = css`
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: -0.05em;
+`;
