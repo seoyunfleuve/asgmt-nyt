b65e9bdd522b6dd914db98c6e402ee17f2350b03
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-19 08:36:29 +0900

    Modify : FilterBtn 스타일 수정

--- src/styles/Filter.style.ts
+++ src/styles/Filter.style.ts
@@ -1,57 +1,22 @@
 import { css } from 'styled-components';
 
-export const headerFilter = css`
+export const headerFilterDefault = css`
   color: ${(props) => props.theme.colors.fontGray};
   border: 1px solid ${(props) => props.theme.colors.borderGray};
 `;
 
-export const headerFilterHover = css`
+export const headerFilterActive = css`
   color: ${(props) => props.theme.colors.pointBlue};
   border: 1px solid ${(props) => props.theme.colors.subBlue};
 `;
 
-export const headerFilterActive = css`
-  ${headerFilterHover}
-
-  &:hover {
-    color: #ffffff;
-    border: 1px solid ${(props) => props.theme.colors.pointBlue};
-    background: ${(props) => props.theme.colors.pointBlue};
-  }
-`;
-
-export const headerFilterDefault = css`
-  ${headerFilter}
-
-  &:hover {
-    ${headerFilterHover}
-  }
-`;
-
-export const countryFilter = css`
+export const countryFilterDefault = css`
   color: ${(props) => props.theme.colors.fontGray};
   border: 1px solid ${(props) => props.theme.colors.borderLightGray};
 `;
 
-export const countryFilterHover = css`
+export const countryFilterActive = css`
   color: #ffffff;
   border: 1px solid ${(props) => props.theme.colors.borderLightGray};
   background: ${(props) => props.theme.colors.subBlue};
-`;
-
-export const countryFilterActive = css`
-  ${countryFilterHover}
-
-  &:hover {
-    border: 1px solid ${(props) => props.theme.colors.pointBlue};
-    background: ${(props) => props.theme.colors.pointBlue};
-  }
-`;
-
-export const countryFilterDefault = css`
-  ${countryFilter}
-
-  &:hover {
-    ${countryFilterHover}
-  }
 `;
