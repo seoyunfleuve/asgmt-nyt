b6d9fc89cd6e5257bf0afac1e2f863d384a1b40d
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-18 23:26:41 +0900

    Add : ArticleList 레이아웃 구성

---
+++ src/organisms/content/ArticleList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ArticleCard from '../../molecules/ArticleCard';
+import { CardContainer } from '../../atoms/Container';
+
+export default function ArticleList() {
+  return (
+    <CardContainer>
+      <ArticleCard />
+    </CardContainer>
+  );
+}
