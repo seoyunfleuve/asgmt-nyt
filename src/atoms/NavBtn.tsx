8ed5f871a232050e9d2e7ec8b7b16363265d4093
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-18 18:31:13 +0900

    Add : NavBtn 컴포넌트 추가

---
+++ src/atoms/NavBtn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function NavBtn() {
+  return <PageBtn />;
+}
+
+const PageBtn = styled.button`
+  width: 24px;
+  height: 45px;
+  background: #ffffff;
+`;
