fd7536da81f12d01c2bc01f95e7d3d481dbaf4a6
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-18 16:52:37 +0900

    Add : theme (height) 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,24 @@
 import { DefaultTheme } from 'styled-components';
 
+const colors = {
+  bgGray: '#F0F1F4',
+  fontGray: '#6D6D6D',
+  borderGray: '#C4C4C4',
+  borderLightGray: '#F2F2F2',
+  pointBlue: '#3478F6',
+  subBlue: '#82B0F4',
+};
+
+const height = {
+  headerHeight: '60px',
+  filterHeight: '34px',
+  navHeight: '85px',
+  cardHeight: '104px',
+};
+
 const theme: DefaultTheme = {
-  colors: {
-    bgGray: '#F0F1F4',
-    fontGray: '#6D6D6D',
-    borderGray: '#C4C4C4',
-    borderLightGray: '#F2F2F2',
-    pointBlue: '#3478F6',
-    subBlue: '#82B0F4',
-  },
+  colors,
+  height,
 };
 
 export default theme;
