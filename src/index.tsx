1b06509a294983eb2db68c7185b95f114cb56d7d
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-19 21:27:41 +0900

    Add : React Query 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from './Router';
 import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
@@ -11,10 +13,14 @@ const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = createRoot(rootElement);
+const queryClient = new QueryClient();
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <RouterProvider router={router} />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
