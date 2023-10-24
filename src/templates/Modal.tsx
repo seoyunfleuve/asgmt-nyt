30c774367a8c2b3d455151bfb236c61f62e59332
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-24 22:32:19 +0900

    Add : Modal 컴포넌트 추가 (modalContent 렌더링, useSearchParams 적용)

---
+++ src/templates/Modal.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { ModalBackground } from '../atoms/Container';
+import { useModalState } from '../store/toggleStore';
+import useModalStore from '../store/useModalStore';
+
+export default function Modal() {
+  const [params, setParams] = useSearchParams();
+
+  const { setIsToggle: setIsModalToggle } = useModalState();
+  const { modalContent } = useModalStore();
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const modalOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (modalRef.current === e.target) {
+      e.stopPropagation();
+      setIsModalToggle(false);
+    }
+  };
+
+  useEffect(() => {
+    params.set('modal', 'true');
+    setParams(params);
+
+    return () => {
+      params.delete('modal');
+      setParams(params);
+    };
+  }, []);
+
+  return (
+    <ModalBackground ref={modalRef} onClick={modalOutsideClick}>
+      {modalContent}
+    </ModalBackground>
+  );
+}
