36733327e36d44a4e4ea41282a3d4308fe16c9e8
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-24 22:43:26 +0900

    Add : useGetFilterArticle 추가 및 isFilterToggle 에 따른 동적 데이터 요청

--- src/pages/HomeScrap.tsx
+++ src/pages/HomeScrap.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ArticleList from '../templates/ArticleList';
-import { useGetNytArticle } from '../api/articleSearchAPI';
+import {
+  useGetFilteredArticle,
+  useGetNytArticle,
+} from '../api/articleSearchAPI';
+import NoData from '../molecules/NoData';
+import { useArticleFilterState } from '../store/toggleStore';
+import useAppliedArticleFilterStore from '../store/useAppliedArticleFilterStore';
 
 export default function HomeScrap() {
-  const { data } = useGetNytArticle();
-  const articleArr = data?.response.docs || [];
+  const { isToggle: isFilterToggle } = useArticleFilterState();
+  const { appliedArticleFilter } = useAppliedArticleFilterStore();
 
-  return <ArticleList articleArr={articleArr} />;
+  const { data: allArticleData } = useGetNytArticle();
+  const { data: filteredData } = useGetFilteredArticle(appliedArticleFilter);
+
+  const data = isFilterToggle ? filteredData : allArticleData;
+
+  const articleArr = data?.response?.docs || [];
+
+  return articleArr.length > 0 ? (
+    <ArticleList articleArr={articleArr} />
+  ) : (
+    <NoData />
+  );
 }
