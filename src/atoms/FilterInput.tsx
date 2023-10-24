817a4422d9d6c835babbd4ce0fc1cdf469fd157d
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-24 22:06:11 +0900

    Add : FilterInput 컴포넌트 수정 (setFilter, type, placeholder props)

--- src/atoms/FilterInput.tsx
+++ src/atoms/FilterInput.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { IFilterInputProps } from '../type/Filter.type';
+import { IInputProps } from '../type/Filter.type';
 import { innerFont } from '../styles/Font.style';
 
-export default function FilterInput({ placeholder }: IFilterInputProps) {
-  return <FInput placeholder={placeholder} />;
+export default function FilterInput({
+  setFilter,
+  type,
+  placeholder,
+}: IInputProps) {
+  const [isClick, setIsClick] = useState(false);
+  return (
+    <FInput
+      type={isClick ? type : 'input'}
+      placeholder={placeholder}
+      onChange={(e) => setFilter && setFilter(e.target.value)}
+      onClick={() => setIsClick(true)}
+    />
+  );
 }
 
-const FInput = styled.input`
+const FInput = styled.input.attrs((props) => ({
+  type: props.type,
+}))`
   width: 100%;
   height: ${(props) => props.theme.height.input};
   background-color: #ffffff;
   padding: 10px 20px 10px 20px;
   border-radius: 8px;
   border: 1px solid ${(props) => props.theme.colors.borderGray};
+  color: ${(props) => props.theme.colors.borderGray};
+  ${innerFont};
+  font-family: 'Apple SD Gothic Neo', sans-serif;
 
   &::placeholder {
     color: ${(props) => props.theme.colors.borderGray};
-    ${innerFont};
   }
 
   &:focus {
