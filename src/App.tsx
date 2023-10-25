5bb702555e702f66094fda86ad873feb866d9cfc
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-25 20:54:33 +0900

    Add : ToastMessage 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Outlet, useSearchParams } from 'react-router-dom';
 import Header from './organisms/Header';
 import NavBar from './organisms/NavBar';
 import { ContentContainer, MobileViewContainer } from './atoms/Container';
-import { useModalState } from './store/toggleStore';
+import { useModalState, useToastState } from './store/useToggleStore';
 import Modal from './templates/Modal';
+import ToastMessage from './atoms/ToastMessage';
 
 function App() {
   const [params] = useSearchParams();
   const { isToggle: isModalToggle, setIsToggle: setIsModalToggle } =
     useModalState();
+  const { isToggle: isToastToggle } = useToastState();
 
   useEffect(() => setIsModalToggle(params.has('modal')), [params]);
 
@@ -19,6 +21,7 @@ function App() {
       <Header />
       <ContentContainer>
         <Outlet />
+        {isToastToggle && <ToastMessage />}
       </ContentContainer>
       <NavBar />
     </MobileViewContainer>
