3bca03638bc6b710b6d218e7b0a86dafa6fc08b2
Seoyun Kang <seoyunfleuve@gmail.com>
2023-10-27 00:30:48 +0900

    Modify : Button.type.ts 수정

--- src/type/Button.type.ts
+++ src/type/Button.type.ts
@@ -1,11 +1,24 @@
+import React from 'react';
 import { ISubfilter } from './Filter.type';
 
 export interface IBtnProps {
+  id?: number;
   btnText?: string | string[] | number;
   isActive?: boolean;
   onClick: () => void;
+  icon?: IIcon[];
 }
 
 export interface ITabProps extends IBtnProps {
   tabList?: ISubfilter[];
+}
+
+interface IIcon {
+  id: number;
+  icon: React.ReactElement | null;
+}
+
+export interface INavBtnProps {
+  type: string;
+  activeScreen: string;
 }
